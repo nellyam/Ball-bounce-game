decbe5b687baca022e4fedaa610c5b2a39f161a6
nellyam <nellynellyam@gmail.com>
2021-02-24 12:39:09 +0100

    ball bounce

---
+++ README.md
@@ -0,0 +1,2 @@
+In this game we program this code to dribble the ball on the platform using any key on a keyboard. When a user presses a key down, it  lifts the ball up. When the user releases the key, the ball should drop.
+
---
+++ main.js
@@ -0,0 +1,17 @@
+ 
+  let ball = document.getElementById('float-circle');
+
+// Write your code below
+function up() {
+ball.style.bottom = '250px';
+}
+
+function down() {
+ball.style.bottom = '50px';
+}
+
+document.onkeydown = up;
+document.onkeyup = down;
+  
+
+
